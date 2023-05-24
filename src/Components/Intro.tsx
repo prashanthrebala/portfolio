c6b41049bdd8c13db24e06af9d8399be82b187f8
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-05-24 18:10:45 -0400

    added letter fadein animation

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,11 +1,17 @@
 import { Avatar, Typography, Stack } from "@mui/material";
+import { LetterComponent } from "./LetterComponent";
 import prashanth_rebala from "../Assets/prashanth_rebala.jpg";
+import scatterBG from "../Assets/polygon-scatter-haikei.svg";
 
 export const Intro = () => {
 	return (
 		<Stack
-			sx={{ bgcolor: "text.primary", color: "#eee" }}
-			direction={{ xs: "column", sm: "row" }}
+			sx={{
+				color: "#eee",
+				backgroundImage: `url(${scatterBG})`,
+				backgroundSize: "cover",
+			}}
+			direction={{ xs: "column", md: "row" }}
 			justifyContent={"center"}
 			alignItems={"center"}
 			gap={3}
@@ -14,7 +20,11 @@ export const Intro = () => {
 			<Avatar src={prashanth_rebala} sx={{ width: "16rem", height: "16rem" }} />
 			<Stack margin={1}>
 				<Typography variant="h5">Hi! I'm</Typography>
-				<Typography variant="h3">Prashanth Rebala</Typography>
+				<Typography variant="h3" sx={{ fontWeight: "bold" }}>
+					{"Prashanth Rebala".split("").map((letter, idx) => (
+						<LetterComponent letter={letter} animationDuration={40 * idx} />
+					))}
+				</Typography>
 			</Stack>
 		</Stack>
 	);
