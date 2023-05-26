0f1bc13b8c65d5d88f9703cdc7fd158814b4c02d
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-05-25 23:19:56 -0400

    added div between sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import "./App.css";
 import { Intro } from "./Components/Intro";
 import { Projects } from "./Components/Projects";
 import { WorkTimeline } from "./Components/WorkTimeline";
+import { Box } from "@mui/material";
+import wave1 from "./Assets/wave-haikei.svg";
 
 function App() {
 	return (
 		<>
 			<Intro />
+			<Box
+				sx={{
+					backgroundImage: `url(${wave1})`,
+					width: "100%",
+					height: "200px",
+					margin: "-1rem 0rem",
+				}}
+			/>
 			<WorkTimeline />
 			<Projects />
 		</>
