88efaedcf1cb1b4db4be79ffc3930e09307fc2e9
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-08 07:48:53 -0400

    GitHub target link fix

--- src/components/Projects/GitHub.tsx
+++ src/components/Projects/GitHub.tsx
@@ -1,31 +1,36 @@
 import { AiOutlineGithub } from "react-icons/ai";
 import { ProjectItem } from "../ProjectItem";
-import { Box } from "@mui/material";
+import { Box, Link } from "@mui/material";
 
 export const GitHub = () => {
 	return (
-		<ProjectItem
-			backgroundColor={"#1e272e"}
-			targetLink="https://github.com/prashanthrebala"
+		<Link
+			href={"https://github.com/prashanthrebala"}
+			target="_blank"
+			rel="noopener noreferrer"
+			underline="none"
+			color="inherit"
 		>
-			<Box
-				display={"flex"}
-				flexDirection={{ md: "column" }}
-				justifyContent={"center"}
-				gap={{ md: 2 }}
-				alignItems={"center"}
-				color={"#F2F2F2"}
-			>
-				<AiOutlineGithub size={120} />
+			<ProjectItem backgroundColor={"#1e272e"}>
 				<Box
-					sx={{
-						width: { xs: "50%", sm: "100%" },
-						textAlign: "center",
-					}}
+					display={"flex"}
+					flexDirection={{ md: "column" }}
+					justifyContent={"center"}
+					gap={{ md: 2 }}
+					alignItems={"center"}
+					color={"#F2F2F2"}
 				>
-					View more on <b>GitHub</b>
+					<AiOutlineGithub size={120} />
+					<Box
+						sx={{
+							width: { xs: "50%", sm: "100%" },
+							textAlign: "center",
+						}}
+					>
+						View more on <b>GitHub</b>
+					</Box>
 				</Box>
-			</Box>
-		</ProjectItem>
+			</ProjectItem>
+		</Link>
 	);
 };
