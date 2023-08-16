652937444cf331d4aadbfe859c7810efecc60373
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-08-16 07:01:33 -0400

    add sample illusia data

---
+++ src/data/illusia.ts
@@ -0,0 +1,29 @@
+export const illusia_posts = [
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/e8a6d789-0b6a-4d58-afc4-fdaeba046dca_4aXSg84-m",
+		description:
+			"medieval german castle, surrounded by mountains, high fantasy, epic, digital art",
+	},
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/21b8ebee-ca94-4a5b-b5df-427ebb7790dc_ukljDlE3w",
+		description: "panda mad scientist mixing sparkling chemicals, digital art",
+	},
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/301d75ba-111c-4327-bedb-3b599edb90ea_mm5_TtWzk",
+		description: "a painting of a fox in the style of Starry Night",
+	},
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/a48b6626-e0e5-4fbe-a441-5f738ab143e7_QZDZYsunMX",
+		description:
+			"a fortune-telling shiba inu reading your fate in a giant hamburger, digital art",
+	},
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/15f2f2ca-7a18-4ade-a145-5a49a13e7234_lqT89IYNu",
+		description: "Portrait of a smiling cyborg woman, peaceful, digital art",
+	},
+	{
+		url: "https://ik.imagekit.io/prashanthrebala/illusia/store/b8d0fa4d-f835-44a5-9d4e-685c10ba5c3f_FAH97L_DS",
+		description:
+			"A hamburger in the shape of a Rubik’s cube, professional food photography",
+	},
+];
