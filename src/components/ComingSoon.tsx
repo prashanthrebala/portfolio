175f347bc73b27e1e504469e1fe00eb193e55826
Prashanth Rebala <prashanth.rebala@gmail.com>
2024-03-02 01:27:30 -0500

    fixed scroll snap issue on mobile

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -12,133 +12,133 @@ import youtube from "../assets/images/youtube.jpeg";
 import youtube_logo from "../assets/images/youtube-logo.png";
 
 export const ComingSoon = () => {
-	const { ref, inView } = useInView({ threshold: 0.6, triggerOnce: true });
-	const { mdScreen } = useContext(BreakpointsContext);
+  const { ref, inView } = useInView({ threshold: 0.6, triggerOnce: true });
+  const { mdScreen } = useContext(BreakpointsContext);
 
-	return (
-		<Grid
-			ref={ref}
-			container
-			sx={{
-				scrollSnapAlign: "start",
-				minHeight: "100vh",
-				backgroundColor: "#146C94",
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "center",
-				alignItems: "center",
-				color: "#DFDFDF",
-				overflow: "hidden",
-			}}
-		>
-			<Typography
-				sx={{
-					backgroundColor: "#146C94",
-					fontSize: { xs: "3em", md: "5em" },
-					fontWeight: 300,
-				}}
-			>
-				Coming Soon
-			</Typography>
-			<Divider
-				sx={{
-					width: "80%",
-					borderColor: "white",
-					marginY: "1.5em",
-				}}
-			/>
-			<Grid
-				container
-				display={"flex"}
-				justifyContent={"center"}
-				alignItems={"center"}
-			>
-				<ComingSoonGridItem inView={inView}>
-					<ImBooks size={mdScreen ? 120 : 80} />
-					Education
-				</ComingSoonGridItem>
-				<ComingSoonGridItem inView={inView}>
-					<Avatar
-						src={instagram}
-						sx={{
-							width: { xs: "5rem", lg: "12rem" },
-							height: { xs: "5rem", lg: "12rem" },
-							border: "0.5em solid #146C94",
-						}}
-					/>
-					{mdScreen ? (
-						<Box
-							sx={{
-								backgroundImage: `url(${instagram_logo})`,
-								backgroundSize: "cover",
-								backgroundRepeat: "no-repeat",
-								backgroundPosition: "center",
-								width: "12rem",
-								height: "4rem",
-							}}
-						/>
-					) : (
-						"Instagram"
-					)}
-				</ComingSoonGridItem>
-				<ComingSoonGridItem inView={inView}>
-					<Avatar
-						src={youtube}
-						sx={{
-							width: { xs: "5rem", lg: "12rem" },
-							height: { xs: "5rem", lg: "12rem" },
-							border: "0.5em solid #146C94",
-						}}
-					/>
-					{mdScreen ? (
-						<Box
-							sx={{
-								backgroundImage: `url(${youtube_logo})`,
-								backgroundSize: "cover",
-								backgroundRepeat: "no-repeat",
-								backgroundPosition: "center",
-								width: "10rem",
-								height: "3rem",
-							}}
-						/>
-					) : (
-						"YouTube"
-					)}
-				</ComingSoonGridItem>
-				<ComingSoonGridItem inView={inView}>
-					<CiCircleMore size={mdScreen ? 120 : 80} />
-					Other
-				</ComingSoonGridItem>
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid
+      ref={ref}
+      container
+      sx={{
+        scrollSnapAlign: { xs: "none", md: "start" },
+        minHeight: "100vh",
+        backgroundColor: "#146C94",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#DFDFDF",
+        overflow: "hidden",
+      }}
+    >
+      <Typography
+        sx={{
+          backgroundColor: "#146C94",
+          fontSize: { xs: "3em", md: "5em" },
+          fontWeight: 300,
+        }}
+      >
+        Coming Soon
+      </Typography>
+      <Divider
+        sx={{
+          width: "80%",
+          borderColor: "white",
+          marginY: "1.5em",
+        }}
+      />
+      <Grid
+        container
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <ComingSoonGridItem inView={inView}>
+          <ImBooks size={mdScreen ? 120 : 80} />
+          Education
+        </ComingSoonGridItem>
+        <ComingSoonGridItem inView={inView}>
+          <Avatar
+            src={instagram}
+            sx={{
+              width: { xs: "5rem", lg: "12rem" },
+              height: { xs: "5rem", lg: "12rem" },
+              border: "0.5em solid #146C94",
+            }}
+          />
+          {mdScreen ? (
+            <Box
+              sx={{
+                backgroundImage: `url(${instagram_logo})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                width: "12rem",
+                height: "4rem",
+              }}
+            />
+          ) : (
+            "Instagram"
+          )}
+        </ComingSoonGridItem>
+        <ComingSoonGridItem inView={inView}>
+          <Avatar
+            src={youtube}
+            sx={{
+              width: { xs: "5rem", lg: "12rem" },
+              height: { xs: "5rem", lg: "12rem" },
+              border: "0.5em solid #146C94",
+            }}
+          />
+          {mdScreen ? (
+            <Box
+              sx={{
+                backgroundImage: `url(${youtube_logo})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                width: "10rem",
+                height: "3rem",
+              }}
+            />
+          ) : (
+            "YouTube"
+          )}
+        </ComingSoonGridItem>
+        <ComingSoonGridItem inView={inView}>
+          <CiCircleMore size={mdScreen ? 120 : 80} />
+          Other
+        </ComingSoonGridItem>
+      </Grid>
+    </Grid>
+  );
 };
 
 interface ComingSoonGridItemProps {
-	children: React.ReactNode;
-	inView: boolean;
+  children: React.ReactNode;
+  inView: boolean;
 }
 
 const ComingSoonGridItem = ({ children, inView }: ComingSoonGridItemProps) => {
-	return (
-		<Zoom in={inView} timeout={400}>
-			<Grid
-				container
-				item
-				xs={5}
-				md={2.5}
-				display={"flex"}
-				justifyContent={"center"}
-				direction={"column"}
-				alignItems={"center"}
-				fontSize={{ xs: "1.3em", md: "2em" }}
-				fontFamily={"Roboto"}
-				fontWeight={300}
-				gap={1}
-				marginY={2}
-			>
-				{children}
-			</Grid>
-		</Zoom>
-	);
+  return (
+    <Zoom in={inView} timeout={400}>
+      <Grid
+        container
+        item
+        xs={5}
+        md={2.5}
+        display={"flex"}
+        justifyContent={"center"}
+        direction={"column"}
+        alignItems={"center"}
+        fontSize={{ xs: "1.3em", md: "2em" }}
+        fontFamily={"Roboto"}
+        fontWeight={300}
+        gap={1}
+        marginY={2}
+      >
+        {children}
+      </Grid>
+    </Zoom>
+  );
 };
