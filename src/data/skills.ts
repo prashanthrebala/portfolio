7b27204e3b841c02f7ac897adcafd8172213cc5a
Prashanth Rebala <prashanth.rebala@gmail.com>
2024-03-02 02:26:55 -0500

    edit skills section

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -22,157 +22,169 @@ import fastapi from "../assets/fastapi.svg";
 import vue from "../assets/vue.svg";
 
 export const skills = {
-	skillSet: [
-		{
-			name: "React",
-			icon: react,
-			height: "5rem",
-		},
-		{
-			name: "NodeJS",
-			icon: node,
-			height: "4rem",
-		},
-		{
-			name: "MongoDB",
-			icon: mongo,
-			height: "2rem",
-		},
-		{
-			name: "Express",
-			icon: express,
-			height: "2rem",
-		},
-		{
-			name: "Typescript",
-			icon: typescript,
-			height: "4rem",
-		},
-		{
-			name: "HTML",
-			icon: html,
-			height: "5rem",
-		},
-		{
-			name: "CSS",
-			icon: css,
-			height: "5rem",
-		},
-		{
-			name: "JavaScript",
-			icon: javascript,
-			height: "4rem",
-		},
-		{
-			name: "GraphQL",
-			icon: graphql,
-			height: "5rem",
-		},
-		{
-			name: "Cypress",
-			icon: cypress,
-			height: "4rem",
-		},
-		{
-			name: "Git",
-			icon: git,
-			height: "5rem",
-		},
-		{
-			name: "Python",
-			icon: python,
-			height: "4rem",
-		},
-		{
-			name: "Java",
-			icon: java,
-			height: "5rem",
-		},
-		{
-			name: "Angular",
-			icon: angular,
-			height: "4rem",
-		},
-		{
-			name: "MySQL",
-			icon: mysql,
-			height: "3rem",
-		},
-		{
-			name: "NextJS",
-			icon: nextjs,
-			height: "4rem",
-		},
-		{
-			name: "Docker",
-			icon: docker,
-			height: "4rem",
-		},
-		{
-			name: "Kubernetes",
-			icon: kubernetes,
-			height: "4rem",
-		},
-		{
-			name: "Tailwind",
-			icon: tailwind,
-			height: "4rem",
-		},
-		{
-			name: "Prisma",
-			icon: prisma,
-			height: "4rem",
-		},
-		{
-			name: "VueJs",
-			icon: vue,
-			height: "5rem",
-		},
-		{
-			name: "FastAPI",
-			icon: fastapi,
-			height: "5rem",
-		},
-	],
-	categories: [
-		{
-			title: "Web Development",
-			items: [
-				"Node.js",
-				"React",
-				"Next.js",
-				"FastAPI",
-				"TypeScript",
-				"Express",
-				"Angular",
-				"Vue.js",
-				"Tailwind CSS",
-				"HTML / CSS / JS",
-			],
-		},
-		{
-			title: "Programming Languages",
-			items: ["JavaScript", "Python", "Java", "C#"],
-		},
-		{
-			title: "Data Management",
-			items: ["MongoDB", "GraphQL", "MySQL", "PostgreSQL", "Prisma"],
-		},
-		// {
-		// 	title: "CSS Frameworks",
-		// 	items: ["Tailwind CSS", "Bootstrap"],
-		// },
-		// {
-		// 	title: "Backend Frameworks",
-		// 	items: ["Django", "Flask"],
-		// },
-		{
-			title: "Tools",
-			items: ["Docker", "Kubernetes", "Git", "GitHub"],
-		},
-		{
-			title: "Testing",
-			items: ["Jest", "Cypress"],
-		},
-	],
+  skillSet: [
+    {
+      name: "React",
+      icon: react,
+      height: "5rem",
+    },
+    {
+      name: "NodeJS",
+      icon: node,
+      height: "4rem",
+    },
+    {
+      name: "MongoDB",
+      icon: mongo,
+      height: "2rem",
+    },
+    {
+      name: "Express",
+      icon: express,
+      height: "2rem",
+    },
+    {
+      name: "Typescript",
+      icon: typescript,
+      height: "4rem",
+    },
+    {
+      name: "HTML",
+      icon: html,
+      height: "5rem",
+    },
+    {
+      name: "CSS",
+      icon: css,
+      height: "5rem",
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+      height: "4rem",
+    },
+    {
+      name: "GraphQL",
+      icon: graphql,
+      height: "5rem",
+    },
+    {
+      name: "Cypress",
+      icon: cypress,
+      height: "4rem",
+    },
+    {
+      name: "Git",
+      icon: git,
+      height: "5rem",
+    },
+    {
+      name: "Python",
+      icon: python,
+      height: "4rem",
+    },
+    {
+      name: "Java",
+      icon: java,
+      height: "5rem",
+    },
+    {
+      name: "Angular",
+      icon: angular,
+      height: "4rem",
+    },
+    {
+      name: "MySQL",
+      icon: mysql,
+      height: "3rem",
+    },
+    {
+      name: "NextJS",
+      icon: nextjs,
+      height: "4rem",
+    },
+    {
+      name: "Docker",
+      icon: docker,
+      height: "4rem",
+    },
+    {
+      name: "Kubernetes",
+      icon: kubernetes,
+      height: "4rem",
+    },
+    {
+      name: "Tailwind",
+      icon: tailwind,
+      height: "4rem",
+    },
+    {
+      name: "Prisma",
+      icon: prisma,
+      height: "4rem",
+    },
+    {
+      name: "VueJs",
+      icon: vue,
+      height: "5rem",
+    },
+    {
+      name: "FastAPI",
+      icon: fastapi,
+      height: "5rem",
+    },
+  ],
+  categories: [
+    {
+      title: "Programming Languages",
+      items: [
+        "JavaScript",
+        "TypeScript",
+        "Python",
+        "Go",
+        "Java",
+        "C#",
+        "C/C++",
+      ],
+    },
+    {
+      title: "Frontend Development",
+      items: [
+        "React",
+        "TypeScript",
+        "Next.js",
+        "Redux",
+        "HTML / CSS / JS",
+        "Tailwind",
+        "Material UI",
+        "Angular",
+        "Vue.js",
+      ],
+    },
+    {
+      title: "Backend Development",
+      items: [
+        "Node.js",
+        "Python",
+        "Go",
+        "Express",
+        "FastAPI",
+        "Django",
+        "RESTful APIs",
+        "Apollo GraphQL",
+      ],
+    },
+    {
+      title: "Utilities",
+      items: ["Docker", "Kubernetes", "Git", "Redis", "AWS", "Unix", "Nginx"],
+    },
+    {
+      title: "Data Management",
+      items: ["SQL and NoSQL", "MySQL", "PostgreSQL", "MongoDB", "CosmosDB"],
+    },
+    {
+      title: "Testing Frameworks",
+      items: ["Jest", "Playwright", "Cypress", "Protractor"],
+    },
+  ],
 };
